4413d038a057a973e0d9a0abd4fbe65798a14198
mikhail-kalugin <Alekseevich.m.k@gmail.com>
2025-02-01 16:35:01 +0300

    add frame

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Service  Service
+	Postgres Postgres
+	Metrics  Metrics
+	Platform Platform
+}
+
+type Service struct {
+	Port string `env:"QR_SERVICE_PORT"`
+	Name string `env:"QR_SERVICE_NAME"`
+}
+
+type Postgres struct {
+	User     string `env:"QR_SERVICE_POSTGRES_USER"`
+	Password string `env:"QR_SERVICE_POSTGRES_PASSWORD"`
+	Database string `env:"QR_SERVICE_POSTGRES_DB"`
+	Host     string `env:"QR_SERVICE_POSTGRES_HOST"`
+	Port     string `env:"QR_SERVICE_POSTGRES_PORT"`
+}
+
+type Metrics struct {
+	Host string `env:"GRAFANA_HOST"`
+	Port int    `env:"GRAFANA_PORT"`
+}
+
+type Platform struct {
+	Env string `env:"ENV"`
+}
+
+func MustLoad() *Config {
+	cfg := &Config{}
+	err := cleanenv.ReadEnv(cfg)
+
+	if err != nil {
+		log.Fatalf("failed to read env variables: %s", err)
+	}
+
+	return cfg
+}
